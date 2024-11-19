19f83aae2acf481126aadd8ea2e06ce9c9628b97
Erik <ErikOverflow+GameDev@gmail.com>
2024-11-18 23:38:46 -0600

    Add animations and general updates to enemy class

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health;
-    public int maxHealth;
-    public int attack;
-    public int defense;
-    public SpriteRenderer spriteRenderer;
     public Monster monster;
-    public TextMeshProUGUI enemyNameText;
+    int health;
+    int maxHealth;
+    int attack;
+    int defense;
+    [SerializeField]
+    SpriteRenderer spriteRenderer;
+    [SerializeField]
+    TextMeshProUGUI enemyNameText;
     [SerializeField]
     RectTransform currentHealthTransform;
     [SerializeField]
     RectTransform maxHealthTransform;
+    [SerializeField]
+    Animator animator;
+    [SerializeField]
+    AnimatorController defaultAnimatorController;
 
     private void OnEnable()
     {
@@ -29,13 +36,18 @@ public class Enemy : MonoBehaviour
 
     public void Initialize(Monster monster)
     {
+        animator.ResetTrigger("Dead");
+        animator.ResetTrigger("Damaged");
         health = monster.health;
         maxHealth = monster.health;
         attack = monster.attack;
         defense = monster.defense;
-        spriteRenderer.sprite = monster.sprite;
         this.monster = monster;
         enemyNameText.text = monster.name;
+        spriteRenderer.sprite = monster.sprite;
+        currentHealthTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxHealthTransform.rect.width * (float)health / maxHealth);
+        animator.runtimeAnimatorController = monster.animatorController ? monster.animatorController : defaultAnimatorController;
+        animator.SetTrigger("Spawned");
     }
 
     private void takeDamage(int damage)
@@ -44,13 +56,19 @@ public class Enemy : MonoBehaviour
             return;
         damage -= defense;
         damage = Mathf.Max(0, damage);
+        if(damage == 0)
+        {
+            return;
+        }
         health -= damage;
         currentHealthTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, maxHealthTransform.rect.width * (float) health / maxHealth);
         if(health <= 0)
         {
+            animator.SetTrigger("Dead");
             Events.enemyDied?.Invoke(monster);
             return;
         }
+        animator.SetTrigger("Damaged");
         Events.enemyAttack?.Invoke(attack);
     }
 }
--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,4 +7,5 @@ public class Monster : ScriptableObject
     public int attack;
     public int defense;
     public Sprite sprite;
+    public AnimatorOverrideController animatorController;
 }
